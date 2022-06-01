9f96504a9d9bdf9566c411f488824eca5822d32d
Treemzy <Treemzy@gmail.com>
2022-06-01 16:54:44 +0100

    Update settings.py

--- CHMS/settings.py
+++ CHMS/settings.py
@@ -129,7 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # added manually
 STATICFILES_DIRS = [
     BASE_DIR / "static",
